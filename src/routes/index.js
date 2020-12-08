4ec17c323b27709c6067a0e4256cc1d1d1fd43c2
Pablo Arrieta <pablou2015@hotmail.com>
2020-12-08 11:57:13 -0500

    first steps

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const { ErrorMiddleware, NotFoundMiddleware } = require('../middlewares');
+require('express-async-errors');
+
+module.exports = ({ HomeRoutes }) => {
+  const router = express.Router();
+  const apiRoutes = express.Router();
+
+  apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
+
+  apiRoutes.use('/home', HomeRoutes);
+
+  router.use('/v1/api', apiRoutes);
+  router.use(ErrorMiddleware);
+  router.use(NotFoundMiddleware);
+
+  return router;
+};
---
+++ src/services/home.service.js
@@ -0,0 +1,7 @@
+class HomeService {
+  index() {
+    message: 'Hello World!'
+  }
+}
+
+module.exports = HomeService
