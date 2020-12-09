ab24176770793eba0864c96d0d4cf63d846b939c
Pablo Arrieta <pablou2015@hotmail.com>
2020-12-09 18:49:02 -0500

    Added user test

---
+++ tests/mocks/user/user.model.mock.js
@@ -0,0 +1,22 @@
+module.exports = {
+  user: {
+    _id: '5fd0622fff32726304dbed7f',
+    name: 'Pablo',
+    username: 'pablo8',
+    __v: 0,
+  },
+  users: [
+    {
+      _id: '5fd04bbba6615c2694a4b679',
+      username: 'pablo',
+      name: 'Pablo',
+      __v: 0,
+    },
+    {
+      _id: '5fd0622fff32726304dbed7f',
+      name: 'Pablo',
+      username: 'pablo8',
+      __v: 0,
+    },
+  ],
+};
