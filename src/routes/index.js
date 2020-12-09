22c7d9892b07e43f2e585dbe99a3891db1d5c45e
Pablo Arrieta <pablou2015@hotmail.com>
2020-12-08 22:14:38 -0500

    Added routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,13 +5,16 @@ const compression = require('compression');
 const { ErrorMiddleware, NotFoundMiddleware } = require('../middlewares');
 require('express-async-errors');
 
-module.exports = ({ HomeRoutes }) => {
+module.exports = ({ HomeRoutes, UserRoutes, IdeaRoutes, CommentRoutes }) => {
   const router = express.Router();
   const apiRoutes = express.Router();
 
   apiRoutes.use(express.json()).use(cors()).use(helmet()).use(compression());
 
   apiRoutes.use('/home', HomeRoutes);
+  apiRoutes.use('/user', UserRoutes);
+  apiRoutes.use('/idea', IdeaRoutes);
+  apiRoutes.use('/comment', CommentRoutes);
 
   router.use('/v1/api', apiRoutes);
   router.use(ErrorMiddleware);
